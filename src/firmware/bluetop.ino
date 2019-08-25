522fc21ae775ea065446bc56c8e277c313c5c0ad
Connor Douthat <7866089+cadouthat@users.noreply.github.com>
2019-08-25 00:36:14 -0600

    First prototype, with bare-bones Web UI

---
+++ src/firmware/bluetop.ino
@@ -0,0 +1,243 @@
+#include <bluefruit.h>
+
+#define FAN_PIN 16
+#define DUMP_PIN 15
+#define TRAY_PIN 7
+#define DRUM_PIN 11
+#define HEAT_PIN 27
+#define TEMP1_PIN A0
+#define TEMP2_PIN A1
+
+#define TEMP_INTERVAL_MS 1000
+#define HEAT_INTERVAL_MS 500
+
+#define HEAT_LEVELS 10
+#define FAN_LEVELS 10
+
+uint8_t BIG_ENDIAN_UUIDS[][16] = {
+  {0x7a,0x61,0x95,0x9e,0x61,0x1c,0x43,0x46,0x9b,0x27,0x69,0x3e,0x3c,0x18,0x63,0x68}, // Service
+  {0x34,0x03,0x30,0x01,0xc8,0x04,0x4c,0x95,0xa1,0x23,0xab,0x26,0x8e,0xee,0xd4,0x7e}, // Drum
+  {0xae,0x79,0xe4,0x2c,0xae,0x7e,0x4e,0xfa,0x8e,0xd8,0x42,0x10,0xfa,0xd6,0x65,0x2f}, // Heat
+  {0x5f,0x71,0x7e,0xa9,0x68,0x3b,0x40,0x50,0x99,0x23,0x32,0xc7,0xfa,0xa7,0xd1,0x1d}, // Fan
+  {0x66,0x62,0x71,0xdc,0x66,0xbe,0x48,0x19,0x90,0x4f,0x83,0x1c,0x6e,0x82,0x5d,0x15}, // Tray
+  {0xf3,0x10,0x28,0xe0,0x41,0xb0,0x48,0xee,0x96,0xed,0xa1,0x70,0x71,0x80,0x18,0x3b}, // Dump
+  {0xe5,0x84,0xef,0x20,0x0f,0x9c,0x45,0xfa,0xbf,0x13,0x3b,0x30,0xcd,0x28,0x40,0x42}, // Temp1
+  {0x01,0x6d,0x72,0x3f,0x5b,0xa7,0x4a,0x62,0xba,0x77,0x4a,0x3f,0x6a,0x08,0x19,0x63}  // Temp2
+};
+uint8_t UUIDS[8][16];
+
+uint8_t* nextUuid() {
+  static int nextConst = 0;
+  uint8_t *in = BIG_ENDIAN_UUIDS[nextConst];
+  uint8_t *out = UUIDS[nextConst];
+  for (int i = 0; i < 16; i++) {
+    out[15 - i] = in[i];
+  }
+  nextConst++;
+  return out;
+}
+
+BLEService        service = BLEService(nextUuid());
+BLECharacteristic drumCh = BLECharacteristic(nextUuid());
+BLECharacteristic heatCh = BLECharacteristic(nextUuid());
+BLECharacteristic fanCh = BLECharacteristic(nextUuid());
+BLECharacteristic trayCh = BLECharacteristic(nextUuid());
+BLECharacteristic dumpCh = BLECharacteristic(nextUuid());
+BLECharacteristic temp1Ch = BLECharacteristic(nextUuid());
+BLECharacteristic temp2Ch = BLECharacteristic(nextUuid());
+
+// Device Information Service
+BLEDis dis;
+
+int heatLevel = 0;
+
+void setup() {
+  // nrf52840 with native usb
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+
+  Serial.println("\nRoast controller booted");
+  Serial.println("-----------------------\n");
+
+  pinMode(DRUM_PIN, OUTPUT);
+  pinMode(HEAT_PIN, OUTPUT); // PWM
+  pinMode(FAN_PIN, OUTPUT); // PWM
+  pinMode(TRAY_PIN, OUTPUT);
+  pinMode(DUMP_PIN, OUTPUT);
+  // TEMP1_PIN is analog input
+  // TEMP2_PIN is analog input
+
+  Bluefruit.begin();
+  Bluefruit.setName("Bluetop");
+  Bluefruit.setConnectCallback(onConnect);
+  Bluefruit.setDisconnectCallback(onDisconnect);
+
+  dis.setManufacturer("Bluetop");
+  dis.setModel("0.0.1");
+  dis.begin();
+
+  Serial.println("Configuring the service");
+  setupService();
+
+  Serial.println("Starting advertising");
+  startAdvertising();
+
+  Serial.println("Setup complete!");
+}
+
+void startAdvertising(void) {
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addService(service);
+  Bluefruit.Advertising.addName();
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  // Units of 0.625ms, fast mode 20ms, slow mode 152.5ms
+  Bluefruit.Advertising.setInterval(32, 244);
+  // Number of seconds in fast mode
+  Bluefruit.Advertising.setFastTimeout(30);
+  // Start with no timeout
+  Bluefruit.Advertising.start(0);
+}
+
+void setupService(void) {
+  service.begin();
+
+  drumCh.setProperties(CHR_PROPS_WRITE);
+  drumCh.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+  drumCh.setFixedLen(1);
+  drumCh.setWriteCallback(onWriteDrum);
+  drumCh.begin();
+
+  heatCh.setProperties(CHR_PROPS_WRITE);
+  heatCh.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+  heatCh.setFixedLen(1);
+  heatCh.setWriteCallback(onWriteHeat);
+  heatCh.begin();
+
+  fanCh.setProperties(CHR_PROPS_WRITE);
+  fanCh.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+  fanCh.setFixedLen(1);
+  fanCh.setWriteCallback(onWriteFan);
+  fanCh.begin();
+
+  trayCh.setProperties(CHR_PROPS_WRITE);
+  trayCh.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+  trayCh.setFixedLen(1);
+  trayCh.setWriteCallback(onWriteTray);
+  trayCh.begin();
+
+  dumpCh.setProperties(CHR_PROPS_WRITE);
+  dumpCh.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+  dumpCh.setFixedLen(1);
+  dumpCh.setWriteCallback(onWriteDump);
+  dumpCh.begin();
+
+  temp1Ch.setProperties(CHR_PROPS_NOTIFY);
+  temp1Ch.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  temp1Ch.setFixedLen(2);
+  temp1Ch.setCccdWriteCallback(onCccd);
+  temp1Ch.begin();
+
+  temp2Ch.setProperties(CHR_PROPS_NOTIFY);
+  temp2Ch.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  temp2Ch.setFixedLen(2);
+  temp2Ch.setCccdWriteCallback(onCccd);
+  temp2Ch.begin();
+}
+
+void onConnect(uint16_t conn_handle) {
+  char central_name[32] = { 0 };
+  Bluefruit.Gap.getPeerName(conn_handle, central_name, sizeof(central_name));
+
+  Serial.print("Connected to ");
+  Serial.println(central_name);
+}
+
+void onDisconnect(uint16_t conn_handle, uint8_t reason) {
+  Serial.println("Disconnected");
+}
+
+void onCccd(BLECharacteristic& chr, uint16_t cccd_value) {
+  Serial.print("CCCD Updated: ");
+  Serial.println(cccd_value);
+}
+
+void onWriteDrum(BLECharacteristic& chr, uint8_t* data, uint16_t len, uint16_t offset) {
+  Serial.print("Drum value updated: ");
+  Serial.println(data[0]);
+
+  digitalWrite(DRUM_PIN, data[0] ? LOW : HIGH);
+}
+void onWriteHeat(BLECharacteristic& chr, uint8_t* data, uint16_t len, uint16_t offset) {
+  Serial.print("Heat value updated: ");
+  Serial.println(data[0]);
+
+  heatLevel = min(HEAT_LEVELS, data[0]);
+}
+void onWriteFan(BLECharacteristic& chr, uint8_t* data, uint16_t len, uint16_t offset) {
+  Serial.print("Fan value updated: ");
+  Serial.println(data[0]);
+
+  int fanPwm = 255 - min(255, round((float)data[0] / FAN_LEVELS * 255));
+  analogWrite(FAN_PIN, fanPwm);
+}
+void onWriteTray(BLECharacteristic& chr, uint8_t* data, uint16_t len, uint16_t offset) {
+  Serial.print("Tray value updated: ");
+  Serial.println(data[0]);
+
+  digitalWrite(TRAY_PIN, data[0] ? LOW : HIGH);
+}
+void onWriteDump(BLECharacteristic& chr, uint8_t* data, uint16_t len, uint16_t offset) {
+  Serial.print("Dump value updated: ");
+  Serial.println(data[0]);
+
+  digitalWrite(DUMP_PIN, data[0] ? LOW : HIGH);
+}
+
+int readTemp(int pin) {
+  analogReference(AR_INTERNAL_3_0);
+  analogReadResolution(12);
+  int analog = analogRead(pin);
+  float v = 3.0 * analog / ((1 << 12) - 1);
+  float tempC = (v - 1.25) / 0.005;
+  float tempF = tempC * 9 / 5 + 32;
+  return tempF;
+}
+
+void loop() {
+  unsigned long curMs = millis();
+
+  static bool heatState = false;
+  unsigned long heatCycle = curMs % (HEAT_INTERVAL_MS * HEAT_LEVELS);
+  if (heatState != (heatCycle < HEAT_INTERVAL_MS * heatLevel)) {
+    heatState = !heatState;
+    Serial.print("Set heat state: ");
+    Serial.println(heatState);
+
+    digitalWrite(HEAT_PIN, heatState ? LOW : HIGH);
+  }
+
+  static int nextTempMs = 0;
+  if (curMs > nextTempMs) {
+    nextTempMs = curMs + TEMP_INTERVAL_MS;
+    int temp1 = readTemp(TEMP1_PIN);
+    int temp2 = readTemp(TEMP2_PIN);
+    Serial.print("Current temp: ");
+    Serial.print(temp1);
+    Serial.print(", ");
+    Serial.println(temp2);
+  
+    if (Bluefruit.connected()) {
+      if (!temp1Ch.notify16(temp1)) {
+        Serial.println("Error: temp1 notify failed!");
+      }
+      if (!temp2Ch.notify16(temp2)) {
+        Serial.println("Error: temp2 notify failed!");
+      }
+    }
+  }
+
+  delay(50);
+}
